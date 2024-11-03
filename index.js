07333dd7407337cafb01cec6faf4812990a08298
Chee Jiann Pung <jiaweipung@gmai.com>
2024-11-03 17:49:41 +0800

    Last commit

--- index.js
+++ index.js
@@ -7,86 +7,53 @@ const viewFavoriteBtn = document.getElementById("favorite-button");
 const apiURL = "https://pokeapi.co/api/v2/pokemon/";
 let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
-const removeFromFavorites = async (pokemonName) => {
+const removeFromFavorites = (pokemonName) => {
   const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
   const updatedFavorites = favorites.filter(
     (pokemon) => pokemon !== pokemonName
   );
   localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
 
-  pokemonDetails.innerHTML = ''
-
-  displayFavorite()
+  displayFavorite();
 };
 
 const displayFavorite = async () => {
   const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-  if(favorites.length > 0){
+  if (favorites.length > 0) {
     pokemonDetails.innerHTML = "<h3>Your Favorite Pokemon</h3>";
-  for (const pokemonName of favorites) {
-    const response = await fetch(apiURL + pokemonName);
-    const pokemonData = await response.json();
-    const pokemonHTML = `
-      <div class="favorite-pokemon">
-        <img src="${pokemonData.sprites.front_default}" alt="${
-      pokemonData.name
-    }">
-        <p>${
-          pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)
-        }</p>
-        <span class="favorite-icon remove-favorite" data-name="${
-          pokemonData.name
-        }">
-        ❤️ 
-      </span>
-      </div>
-    `;
-    pokemonDetails.innerHTML += pokemonHTML;
-  }
+    for (const pokemonName of favorites) {
+      const response = await fetch(apiURL + pokemonName);
+      const pokemonData = await response.json();
+      const pokemonHTML = `
+        <div class="favorite-pokemon">
+          <img src="${pokemonData.sprites.front_default}" alt="${
+        pokemonData.name
+      }">
+          <p>${
+            pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)
+          }</p>
+          <span class="favorite-icon remove-favorite" data-name="${
+            pokemonData.name
+          }">
+          ❤️ 
+        </span>
+        </div>
+      `;
+      pokemonDetails.innerHTML += pokemonHTML;
+    }
 
-  const removeFavorite = document.querySelectorAll(".remove-favorite");
-  console.log(removeFavorite);
-  removeFavorite.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      const pokemonName = event.target.dataset.name;
-      removeFromFavorites(pokemonName);
+    const removeFavorite = document.querySelectorAll(".remove-favorite");
+    console.log(removeFavorite);
+    removeFavorite.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        const pokemonName = event.target.dataset.name;
+        removeFromFavorites(pokemonName);
+      });
     });
-  });
-  }else(
-    pokemonDetails.innerHTML = "<h3>You don't have any favorite pokemon"
-  )
+  } else {
+    pokemonDetails.innerHTML = "<h3>You dont have any favorite pokemon</h3>";
+  }
 };
-//   pokemonDetails.innerHTML = "<h3>Your Favorite Pokemon</h3>";
-//   for (const pokemonName of favorites) {
-//     const response = await fetch(apiURL + pokemonName);
-//     const pokemonData = await response.json();
-//     const pokemonHTML = `
-//       <div class="favorite-pokemon">
-//         <img src="${pokemonData.sprites.front_default}" alt="${
-//       pokemonData.name
-//     }">
-//         <p>${
-//           pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)
-//         }</p>
-//         <span class="favorite-icon remove-favorite" data-name="${
-//           pokemonData.name
-//         }">
-//         ❤️ 
-//       </span>
-//       </div>
-//     `;
-//     pokemonDetails.innerHTML += pokemonHTML;
-//   }
-
-//   const removeFavorite = document.querySelectorAll(".remove-favorite");
-//   console.log(removeFavorite);
-//   removeFavorite.forEach((button) => {
-//     button.addEventListener("click", (event) => {
-//       const pokemonName = event.target.dataset.name;
-//       removeFromFavorites(pokemonName);
-//     });
-//   });
-// };
 
 viewFavoriteBtn.addEventListener("click", displayFavorite);
 
